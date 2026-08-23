ecc3a93aeecaf27d08f23902056f1551cddd51f4
Manthan Chattopadhyay <manthan.chattopadhyay@gmail.com>
2026-08-23 21:31:27 +0200

    Align dispatch base pay summary row

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787513312|5419253';
+const CACHE_VERSION = '1787513485|5545213';
 /** @type {string} */
 const CACHE_PREFIX = 'Pidgu-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
