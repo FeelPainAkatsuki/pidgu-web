a47d521c53ff4e0a0202ac0126d635ab2ff44dfa
Manthan Chattopadhyay <manthan.chattopadhyay@gmail.com>
2026-08-23 21:14:24 +0200

    Replace missing favourite glyph with paper star artwork

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787511913|5925292';
+const CACHE_VERSION = '1787512462|5584616';
 /** @type {string} */
 const CACHE_PREFIX = 'Pidgu-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
