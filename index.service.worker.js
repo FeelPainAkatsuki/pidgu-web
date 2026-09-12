b57d25906542a20d87715e26755939879f348f31
Manthan Chattopadhyay <manthan.chattopadhyay@gmail.com>
2026-09-13 00:21:54 +0200

    Publish refreshed collectibles: seashells and Gen 1

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787516194|6553277';
+const CACHE_VERSION = '1789251708|14220309';
 /** @type {string} */
 const CACHE_PREFIX = 'Pidgu-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
