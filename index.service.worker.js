8ec3fec588cc1455313973f3d7f7905ab0970cb7
Manthan Chattopadhyay <manthan.chattopadhyay@gmail.com>
2026-09-13 01:11:59 +0200

    Publish Pidgu 2026-09-13 01:11

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789251708|14220309';
+const CACHE_VERSION = '1789254715|7075356';
 /** @type {string} */
 const CACHE_PREFIX = 'Pidgu-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
